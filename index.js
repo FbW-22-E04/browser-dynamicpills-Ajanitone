a1e94183b521503b10d5b2a0cae44b241e442508
Ajanitone <ajanitone@freenet.de>
2022-09-24 17:46:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let pills = document.querySelectorAll("li");
+let movies = document.querySelectorAll("section");
+
+let checkAndRemoveClass = function (array, ...classToCheck) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].classList.contains(classToCheck)) {
+      array[i].classList.remove(classToCheck);
+      break;
+    }
+  }
+};
+
+for (let j = 0; j < pills.length; j++) {
+  pills[j].addEventListener("click", function (e) {
+    checkAndRemoveClass(movies, "active");
+    checkAndRemoveClass(pills, "selected");
+    pills[j].classList.add("selected");
+    movies[j].classList.add("active");
+  });
+}
